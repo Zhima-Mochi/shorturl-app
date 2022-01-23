6d57695df93b180723d5a61b05c84979850702cc
Zhima-Mochi <yehgoter@gmail.com>
2022-01-23 16:21:25 +0800

    use aiomysql

--- app/app/main.py
+++ app/app/main.py
@@ -7,7 +7,7 @@ from starlette.responses import FileResponse
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 import aiohttp
 import crud
 import models
@@ -19,9 +19,19 @@ import os
 HOST_NAME = os.environ['HOST_NAME']
 # FRONTEND_HOST_NAME = os.environ['FRONTEND_HOST_NAME']
 
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+
+# @app.on_event("startup")
+# async def startup():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(models.Base.metadata.create_all)
+
+
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await engine.dispose()
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -37,12 +47,12 @@ app.add_middleware(
 # Dependency
 
 
-def get_db():
-    db = SessionLocal()
+async def get_session():
+    session: AsyncSession = SessionLocal()
     try:
-        yield db
+        yield session
     finally:
-        db.close()
+        await session.close()
 
 
 async def valid_url(url):
@@ -66,18 +76,18 @@ async def favicon():
 
 
 @app.get("/{code}")
-async def redirect_to_original_url(code: str, db: Session = Depends(get_db)):
-    return RedirectResponse(url=crud.get_longUrl(db, code))
+async def redirect_to_original_url(code: str, session: AsyncSession = Depends(get_session)):
+    return RedirectResponse(url=await crud.get_longUrl(session, code))
 
 
 @app.post("/")
-async def generate_code(given_url_obj: schemas.OrigUrl, db: Session = Depends(get_db)):
+async def generate_code(given_url_obj: schemas.OrigUrl, session: AsyncSession = Depends(get_session)):
     if(not await valid_url(given_url_obj.url)):
         return {'is_success': False, 'message': '這是一個無效的網址'}
     print(given_url_obj)
-    code_elem = crud.get_code(db, given_url_obj.url)
+    code_elem = await crud.get_code(session, given_url_obj.url)
     if not code_elem:
-        code_elem = crud.create_code(db, given_url_obj.url)
+        code_elem = await crud.create_code(session, given_url_obj.url)
     if code_elem['status']:
         return {'is_success': True, 'host_name': HOST_NAME, 'code': code_elem['code']}
     else:
