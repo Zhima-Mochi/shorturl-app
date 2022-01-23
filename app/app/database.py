2186efda2b31f3ce2dd56dee4856dbf99cc0ce9b
Zhima-Mochi <yehgoter@gmail.com>
2022-01-23 19:24:00 +0800

    change aiomysql to aiosqlite for not well supported to sqlalchemy

--- app/app/database.py
+++ app/app/database.py
@@ -6,16 +6,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 
-# get variables from environment variables
-HOST = os.environ["DB_HOST"]
-DATABASE_NAME = os.environ['DB_NAME']
-USER_NAME = os.environ['DB_USERNAME']
-PASSWORD = get_docker_secret('db_shorturl_password')
+# # get variables from environment variables
+# HOST = os.environ["DB_HOST"]
+# DATABASE_NAME = os.environ['DB_NAME']
+# USER_NAME = os.environ['DB_USERNAME']
+# PASSWORD = get_docker_secret('db_shorturl_password')
 
-SQLALCHEMY_DATABASE_URL = f"mysql+aiomysql://{USER_NAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}?charset=utf8"
+# SQLALCHEMY_DATABASE_URL = f"mysql+aiomysql://{USER_NAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}?charset=utf8"
+SQLALCHEMY_DATABASE_URL = f"sqlite+aiosqlite:///sqlite3/sqlite3.db"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
-#  echo=True
 
 SessionLocal = sessionmaker(
     engine, expire_on_commit=False, autocommit=False, class_=AsyncSession)
